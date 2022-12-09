96d270e81d2520e1ba2b701bed90e66f95d2c17d
epiglotis <83986620+epiglotis@users.noreply.github.com>
2022-12-09 21:38:52 +0300

    Implemented Sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
-import Layout from './components/Layout';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,8 @@
+import Sidebar from "../Sidebar/Sidebar";
+import "./Layout.scss";
+
+const Layout = () => {
+    return <Sidebar></Sidebar>
+}
+
+export default Layout;
