e68f4c73fdebb4e61bfa2c7eb27fe4af465c95a2
epiglotis <83986620+epiglotis@users.noreply.github.com>
2023-10-31 21:38:58 +0300

    Cube Added to Contact and HomePage

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,10 +4,19 @@ import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import './Contact.scss'
 import emailjs from '@emailjs/browser'
 import AlertPopUp from '../Alert/Alert'
+import {
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+  faSass,
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  const [isSuccessful, setIsSuccessful] = useState(false);
+  const [isSuccessful, setIsSuccessful] = useState(false)
   const refForm = useRef()
   const ContactText = 'Contact Me'.split('')
   useEffect(() => {
@@ -89,13 +98,37 @@ const Contact = () => {
                 </li>
               </ul>
             </form>
-            
           </div>
-          {isSuccessful && <AlertPopUp/>} 
+          {isSuccessful && <AlertPopUp />}
         </div>
-        
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faSass} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon
+                icon={faHtml5}
+                style={{ transform: 'rotate(-90deg)' }}
+                color="#F06529"
+              />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
       </div>
-      
+
       <Loader type="pacman" />
     </>
   )
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,6 +4,15 @@ import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import './HomePage.scss'
+import {
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+  faSass,
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -45,12 +54,38 @@ const Home = () => {
             />
           </h1>
           <h2>
-            Javascript / Typescript / HTML / CSS / Express.js / Node.js
-            / Redux / MongoDB / Next.js / Sass / React
+            Javascript / Typescript / HTML / CSS / Express.js / Node.js / Redux
+            / MongoDB / Next.js / Sass / React
           </h2>
           <Link to="/contact" className="flat-button">
             CONTACT ME
           </Link>
+        </div>
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faSass} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon
+                icon={faHtml5}
+                style={{ transform: 'rotate(-90deg)' }}
+                color="#F06529"
+              />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
         </div>
       </div>
       <Loader type="pacman" />
